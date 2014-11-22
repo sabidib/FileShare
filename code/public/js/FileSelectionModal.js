c4de186b087bad63e95c47894c2e6b20b66056a3
Samy Abidib <samy@cenelle.com>
2014-11-22 17:13:27 -0500

    Added FileSelectionModal and duallistbox that should have been in last commit\!

---
+++ code/public/js/FileSelectionModal.js
@@ -0,0 +1,51 @@
+var FileSelectionModal = function FileSelectionModal(model,view){
+	this.view = view;
+	this.model = model;
+	this.tempCallback = {};
+	this.addListeners();
+}
+
+
+
+FileSelectionModal.prototype.addListeners = function(){
+
+	var modal = this;
+	$('body').on('click','#shareGroupChoosingModal #submitShareWithGlobalGroup',function(e){
+		e.preventDefault();
+		var groups = [];
+		$("#shareGroupDualListBox option").each(function()
+		{
+		    groups.push($(this).val());
+		});
+		modal.tempCallback({'shareGroups': groups});
+		modal.tempCallback = {};
+		$('#shareGroupDualListBox').empty();
+		$('#shareGroupDualListBox').bootstrapDualListbox('refresh')
+		$('#shareGroupChoosingModal').modal('hide');
+
+	});
+
+	$('body').on('click','#shareGroupChoosingModal #submitShareWith',function(e){
+		e.preventDefault();	
+		var groups = $('#shareGroupDualListBox').val();
+		modal.tempCallback({'shareGroups':groups});
+		modal.tempCallback = {};
+		$('#shareGroupDualListBox').empty();
+		$('#shareGroupDualListBox').bootstrapDualListbox('refresh')
+		$('#shareGroupChoosingModal').modal('hide');
+	});
+}
+
+
+FileSelectionModal.prototype.getShareGroupsToShareWith = function(files,callback){
+	this.tempCallback = callback; 
+	view.showFilesAboutToBeShared(files);
+	model.getAllShareGroups(function(data){
+		view.showShareGroupsOnlineToChooseFrom(data['shareGroups']);
+	})
+}
+
+
+
+
+
